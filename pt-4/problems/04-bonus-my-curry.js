4c5d413a31b86758d2114b5e273dd183e240d4b0
DoctorDoofen <and.bennett0930@gmail.com>
2024-04-08 16:15:04 -0500

    04 solved

--- pt-4/problems/04-bonus-my-curry.js
+++ pt-4/problems/04-bonus-my-curry.js
@@ -18,8 +18,26 @@
 
 */
 
-// Your code here 
+const myCurry = (func, numArgs) => {
+  let args = [];
+  return gathred = function (...arg) {
+    args.push(...arg);
 
+    if (args.length < numArgs) {
+      console.log(args.length)
+      return gathred;
+    }
+    return func(...args);
+  }
+}
+
+const sumFunc = (...nums) => nums.reduce((acc, el) => acc + el);
+const curriedFunc = myCurry(sumFunc, 4);
+
+console.log(curriedFunc(1)); // returns a function
+console.log(curriedFunc(2)); // returns a function
+console.log(curriedFunc(3)); // returns a function
+console.log(curriedFunc(4)); // doesn't return a sum until the nth function call ==> 10
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = myCurry;
